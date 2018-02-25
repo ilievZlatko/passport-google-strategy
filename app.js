f3fe7eaf3042d86f4856bf0b53aa11c6c93e13ab
Zlatko Iliev <iliev.zlatko@gmail.com>
2018-02-25 21:23:23 +0200

    commit current progress

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
-const express = require('express');
-const authRoutes = require('./routes/auth-routes');
-const passportSetup = require('./config/passport-setup');
+const express = require("express");
+const authRoutes = require("./routes/auth-routes");
+const passportSetup = require("./config/passport-setup");
 const app = express();
-const mongoose = require('mongoose');
-const keys = require('./config/keys');
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 
 // set up view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+
+app.use(
+  cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [keys.session.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/auth", authRoutes);
 
 // connect to mongodb
 mongoose.connect(keys.mongodb.dbURI, () => {
-    console.log('connected to mongoDB');
+  console.log("connected to mongoDB");
 });
 
-app.use('/auth', authRoutes);
-
 // create home route
-app.get('/', (req, res) => {
-    res.render('home');
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
 app.listen(3000, () => {
-    console.log('app listetning for requests on port 3000');
+  console.log("app listetning for requests on port 3000");
 });
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,34 +1,44 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20');
-const keys = require('./keys');
-const User = require('../models/user-model');
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+const keys = require("./keys");
+const User = require("../models/user-model");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(
-    new GoogleStrategy(
-        {
-            // Strategy options
-            callbackURL: '/auth/google/redirect',
-            clientID: keys.google.clientID,
-            clientSecret: keys.google.clientSecret
-        },
-        (accessToken, refreshToken, profile, done) => {
-            // check if user already exist in our db
-            User.findOne({ googleId: profile.id }).then(currentUser => {
-                if (currentUser) {
-                    // user exist
-                    console.log('user is: ', currentUser);
-                } else {
-                    // user not found so we create new one
-                    new User({
-                        username: profile.displayName,
-                        googleId: profile.id
-                    })
-                        .save()
-                        .then(newUser => {
-                            console.log('new user created: ' + newUser);
-                        });
-                }
+  new GoogleStrategy(
+    {
+      // Strategy options
+      callbackURL: "/auth/google/redirect",
+      clientID: keys.google.clientID,
+      clientSecret: keys.google.clientSecret
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // check if user already exist in our db
+      User.findOne({ googleId: profile.id }).then(currentUser => {
+        if (currentUser) {
+          // user exist
+          done(null, currentUser);
+        } else {
+          // user not found so we create new one
+          new User({
+            username: profile.displayName,
+            googleId: profile.id
+          })
+            .save()
+            .then(newUser => {
+              done(null, newUser);
             });
         }
-    )
+      });
+    }
+  )
 );
